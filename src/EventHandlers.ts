ee7347f4b4473331d37657387c91061fb700204c
gwilldan <sayhi2godswill@gmail.com>
2025-05-08 23:26:21 +0100

    update: add nft mint events

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,35 +1,27 @@
-import {
-  AmertisRouter,
-  Swap,
-  Account,
-} from "generated";
-
+import { AmertisRouter, Swap, Account } from "generated";
 
 AmertisRouter.AmertisSwap.handlerWithLoader({
-  loader: async ({ event, context }) => {
-    const account = await context.Account.get(event.transaction.from as string);
-    return { account };
-  },
-  handler: async ({ event, context, loaderReturn }) => {
+	loader: async ({ event, context }) => {
+		const account = await context.Account.get(event.transaction.from as string);
+		return { account };
+	},
+	handler: async ({ event, context, loaderReturn }) => {
+		if (!loaderReturn.account) {
+			const entity: Account = {
+				id: event.transaction.from as string,
+			};
+			context.Account.set(entity);
+		}
 
-    if (!loaderReturn.account) {
-      const entity: Account = {
-        id: event.transaction.from as string,
-      };
-      context.Account.set(entity);
-    }
-
-    const entity: Swap = {
-      id: event.transaction.hash,
-      _tokenIn: event.params._tokenIn,
-      _tokenOut: event.params._tokenOut,
-      _amountIn: event.params._amountIn,
-      _amountOut: event.params._amountOut,
-      from: event.transaction.from as string,
-      timeStamp: event.block.timestamp as unknown as bigint,
-    };
-    context.Swap.set(entity);
-  },
+		const entity: Swap = {
+			id: event.transaction.hash,
+			_tokenIn: event.params._tokenIn,
+			_tokenOut: event.params._tokenOut,
+			_amountIn: event.params._amountIn,
+			_amountOut: event.params._amountOut,
+			from: event.transaction.from as string,
+			timeStamp: event.block.timestamp as unknown as bigint,
+		};
+		context.Swap.set(entity);
+	},
 });
-
-
