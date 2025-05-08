2b5af719033f1589208a062b36d3c38c0f30f281
gwilldan <sayhi2godswill@gmail.com>
2025-05-09 00:06:05 +0100

    fix

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,67 +1,62 @@
-import {
-  AmertisRouter,
-  Swap,
-  Account,
-  Token,
-} from "generated";
+import { AmertisRouter, Swap, Account, Token } from "generated";
 import { getTokenData } from "./utils";
 
 AmertisRouter.AmertisSwap.handlerWithLoader({
-  loader: async ({ event, context }) => {
+	loader: async ({ event, context }) => {
+		const [account, tokenIn, tokenOut] = await Promise.all([
+			context.Account.get(event.transaction.from as string),
+			context.Token.get(event.params._tokenIn as string),
+			context.Token.get(event.params._tokenOut as string),
+		]);
 
-    const [account, tokenIn, tokenOut] = await Promise.all([
-      context.Account.get(event.transaction.from as string),
-      context.Token.get(event.params._tokenIn as string),
-      context.Token.get(event.params._tokenOut as string),
-    ]);
+		return { account, tokenIn, tokenOut };
+	},
+	handler: async ({ event, context, loaderReturn }) => {
+		if (!loaderReturn.account) {
+			const entity: Account = {
+				id: event.transaction.from as string,
+			};
+			context.Account.set(entity);
+		}
 
-    return { account, tokenIn, tokenOut };
-  },
-  handler: async ({ event, context, loaderReturn }) => {
+		if (!loaderReturn.tokenIn) {
+			const tokenData = await getTokenData(
+				event.params._tokenIn as `0x${string}`
+			);
 
-    if (!loaderReturn.account) {
-      const entity: Account = {
-        id: event.transaction.from as string,
-      };
-      context.Account.set(entity);
-    }
+			const entity: Token = {
+				id: event.params._tokenIn as string,
+				symbol: tokenData?.symbol as string,
+				name: tokenData?.name as string,
+				decimals: tokenData?.decimals as unknown as bigint,
+			};
+			context.Token.set(entity);
+		}
 
-    if (!loaderReturn.tokenIn) {
+		if (!loaderReturn.tokenOut) {
+			const tokenData = await getTokenData(
+				event.params._tokenOut as `0x${string}`
+			);
+			const entity: Token = {
+				id: event.params._tokenOut as string,
+				symbol: tokenData?.symbol as string,
+				name: tokenData?.name as string,
+				decimals: tokenData?.decimals as unknown as bigint,
+			};
+			context.Token.set(entity);
+		}
 
-      const tokenData = await getTokenData(event.params._tokenIn as `0x${string}`);
-
-      const entity: Token = {
-        id: event.params._tokenIn as string,
-        symbol: tokenData?.symbol as string,
-        name: tokenData?.name as string,
-        decimals: tokenData?.decimals as unknown as bigint,
-      };
-      context.Token.set(entity);
-    }
-
-    if (!loaderReturn.tokenOut) {
-      const tokenData = await getTokenData(event.params._tokenOut as `0x${string}`);
-      const entity: Token = {
-        id: event.params._tokenOut as string,
-        symbol: tokenData?.symbol as string,
-        name: tokenData?.name as string,
-        decimals: tokenData?.decimals as unknown as bigint,
-      };
-      context.Token.set(entity);
-    }
-
-    const entity: Swap = {
-
-      id: event.transaction.hash,
-      _tokenIn: event.params._tokenIn,
-      _tokenOut: event.params._tokenOut,
-      _amountIn: event.params._amountIn,
-      _amountOut: event.params._amountOut,
-      from: event.transaction.from as string,
-      timeStamp: event.block.timestamp as unknown as bigint,
-      tokenInDetails_id: event.params._tokenIn as string,
-      tokenOutDetails_id: event.params._tokenOut as string,
-    };
-    context.Swap.set(entity);
-  },
+		const entity: Swap = {
+			id: event.transaction.hash,
+			_tokenIn: event.params._tokenIn,
+			_tokenOut: event.params._tokenOut,
+			_amountIn: event.params._amountIn,
+			_amountOut: event.params._amountOut,
+			from: event.transaction.from as string,
+			timeStamp: event.block.timestamp as unknown as bigint,
+			tokenInDetails_id: event.params._tokenIn as string,
+			tokenOutDetails_id: event.params._tokenOut as string,
+		};
+		context.Swap.set(entity);
+	},
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,57 +1,57 @@
 import { createPublicClient, http, Chain, erc20Abi } from "viem";
 
 const monad: Chain = {
-    id: 10143,
-    name: "Monad",
-    nativeCurrency: {
-        name: "Monad",
-        symbol: "MONAD",
-        decimals: 18,
-    },
-    rpcUrls: {
-        default: {
-            http: ["https://testnet-rpc.monad.xyz"],
-        },
-    },
-}
+	id: 10143,
+	name: "Monad",
+	nativeCurrency: {
+		name: "Monad",
+		symbol: "MONAD",
+		decimals: 18,
+	},
+	rpcUrls: {
+		default: {
+			http: ["https://testnet-rpc.monad.xyz"],
+		},
+	},
+};
 
 export const client = createPublicClient({
-    chain: monad,
-    transport: http(),
+	chain: monad,
+	transport: http(),
 });
 
 export const getTokenData = async (address: `0x${string}`) => {
-    try {
-        const data = await client.multicall({
-            contracts: [
-                {
-                    abi: erc20Abi,
-                    address,
-                    functionName: "symbol",
-                },
-                {
-                    abi: erc20Abi,
-                    address,
-                    functionName: "name",
-                },
-                {
-                    abi: erc20Abi,
-                    address,
-                    functionName: "decimals",
-                }
-            ],
-            multicallAddress:
-                "0xcA11bde05977b3631167028862bE2a173976CA11" as `0x${string}`,
-        })
+	try {
+		const data = await client.multicall({
+			contracts: [
+				{
+					abi: erc20Abi,
+					address,
+					functionName: "symbol",
+				},
+				{
+					abi: erc20Abi,
+					address,
+					functionName: "name",
+				},
+				{
+					abi: erc20Abi,
+					address,
+					functionName: "decimals",
+				},
+			],
+			multicallAddress:
+				"0xcA11bde05977b3631167028862bE2a173976CA11" as `0x${string}`,
+		});
 
-        const [symbol, name, decimals] = data
-        const tokenData = {
-            symbol: symbol.result,
-            name: name.result,
-            decimals: decimals.result,
-        }
-        return tokenData
-    } catch (error) {
-        console.error(error)
-    }
-}
+		const [symbol, name, decimals] = data;
+		const tokenData = {
+			symbol: symbol.result,
+			name: name.result,
+			decimals: decimals.result,
+		};
+		return tokenData;
+	} catch (error) {
+		console.error(error);
+	}
+};
